c828dc4ff45b84b7f2a8a389589cac4dee44d058
anshu638 <anshu@nextsavy.com>
2025-03-06 16:34:33 +0530

    Ember movement
    horizontal movement added, ember faces in direction of movement

--- lib/ember_quest/ember_quest_game.dart
+++ lib/ember_quest/ember_quest_game.dart
@@ -1,4 +1,5 @@
 import 'package:flame/components.dart';
+import 'package:flame/events.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 
@@ -9,7 +10,7 @@ import 'objects/ground_block.dart';
 import 'objects/platform_block.dart';
 import 'objects/star.dart';
 
-class EmberQuestGame extends FlameGame {
+class EmberQuestGame extends FlameGame with HasKeyboardHandlerComponents {
   EmberQuestGame();
 
   late EmberPlayer _ember;
