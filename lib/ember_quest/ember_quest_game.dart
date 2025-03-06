84bdd728d6fe72ea495620522cf1885e638a93aa
anshu638 <anshu@nextsavy.com>
2025-03-06 13:14:56 +0530

    world building
    components, assets and grid implemented, normal player added

---
+++ lib/ember_quest/ember_quest_game.dart
@@ -0,0 +1,64 @@
+import 'package:flame/components.dart';
+import 'package:flame/game.dart';
+
+import 'actors/ember_player.dart';
+import 'actors/water_enemy.dart';
+import 'managers/segment_manager.dart';
+import 'objects/ground_block.dart';
+import 'objects/platform_block.dart';
+import 'objects/star.dart';
+
+class EmberQuestGame extends FlameGame {
+  EmberQuestGame();
+
+  late EmberPlayer _ember;
+  double objectSpeed = 0.0;
+
+  @override
+  Future<void> onLoad() async {
+    await images.loadAll([
+      'block.png',
+      'ember.png',
+      'ground.png',
+      'heart_half.png',
+      'heart.png',
+      'star.png',
+      'water_enemy.png',
+    ]);
+    // Everything in this tutorial assumes that the position
+    // of the `CameraComponent`s viewfinder (where the camera is looking)
+    // is in the top left corner, that's why we set the anchor here
+    camera.viewfinder.anchor = Anchor.topLeft;
+    initializeGame();
+  }
+
+  void loadGameSegments(int segmentIndex, double xPositionOffset) {
+    for (final block in segments[segmentIndex]) {
+      switch (block.blockType) {
+        case const (GroundBlock):
+        case const (PlatformBlock):
+          add(PlatformBlock(
+            gridPosition: block.gridPosition,
+            xOffset: xPositionOffset,
+          ));
+        case const (Star):
+        case const (WaterEnemy):
+      }
+    }
+  }
+
+  void initializeGame() {
+    // Assume that size.x < 3200
+    final segmentsToLoad = (size.x / 640).ceil();
+    segmentsToLoad.clamp(0, segments.length);
+
+    for (var i = 0; i <= segmentsToLoad; i++) {
+      loadGameSegments(i, (640 * i).toDouble());
+    }
+
+    _ember = EmberPlayer(
+      position: Vector2(128, canvasSize.y - 70),
+    );
+    world.add(_ember);
+  }
+}
