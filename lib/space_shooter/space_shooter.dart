1266bb1875f630df498a4f2da544f1a36bf4e18a
anshu638 <anshu@nextsavy.com>
2025-03-07 12:26:26 +0530

    Bullet addition
    Bullet asset added, bullet animation, bullet events and time intervals added

--- lib/space_shooter/space_shooter.dart
+++ lib/space_shooter/space_shooter.dart
@@ -31,4 +31,14 @@ class SpaceShooterGame extends FlameGame with PanDetector {
   void onPanUpdate(DragUpdateInfo info) {
     player.move(info.delta.global);
   }
+
+  @override
+  void onPanStart(DragStartInfo info) {
+    player.startShooting();
+  }
+
+  @override
+  void onPanEnd(DragEndInfo info) {
+    player.stopShooting();
+  }
 }
